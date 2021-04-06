bc624d23d6bf5388c2511dcfa4e456a25df6a3dc
bijeshofficial <bijeshr.kunwar@gmail.com>
2021-04-06 21:51:49 +0545

    finished feedback crud using ajax

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.feedbacks, name='feedback'),
     path('create/', views.create_feedback, name='create_feedback'),
+    path('delete/', views.delete_feedback, name='delete_feedback'),
+    path('update/', views.update_feedback, name='update_feedback'),
 ]
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from .forms import FeedBackForm
 from .models import Feedback
@@ -20,13 +20,38 @@ def create_feedback(request):
     if request.method == "POST":
         form = FeedBackForm(request.POST)
         if form.is_valid():
+            feedback_id = request.POST.get('feedbackId')
             title = request.POST.get('title')
             description = request.POST.get('description')
             user = request.user
-            feedback = Feedback(author=user, title=title,
-                                description=description)
+            print(feedback_id)
+            if feedback_id == "":
+                feedback = Feedback(author=user, title=title,
+                                    description=description)
+            else:
+                feedback = Feedback(id=feedback_id, author=user, title=title,
+                                    description=description)
             feedback.save()
             feedback_list = list(Feedback.objects.filter(
                 author_id=request.user.id).values())
             return JsonResponse({'feedback_list': feedback_list})
         return JsonResponse({'status': 'Error'})
+
+
+def delete_feedback(request):
+    if request.method == "POST":
+        id = request.POST.get('feedback_id')
+        feedback = Feedback.objects.get(pk=id)
+        feedback.delete()
+        return JsonResponse({'status': 'Success'})
+    return JsonResponse({'status': 'Error'})
+
+
+def update_feedback(request):
+    if request.method == "POST":
+        id = request.POST.get('feedback_id')
+        feedback = Feedback.objects.get(pk=id)
+        user = request.user.username
+        feedback_data = {"id": feedback.id, "author": user,
+                         "title": feedback.title, "description": feedback.description}
+        return JsonResponse(feedback_data)
